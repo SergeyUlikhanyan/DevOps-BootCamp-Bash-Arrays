d323e68db735f10f14667bd6d865185c7b367bb0
Sergey <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-23 15:12:09 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-x=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-y="$1 folders created:"
+array=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+output="$1 folders created:"
 
-for ((i=0; i<$1; i++)); do
-        mkdir $PWD/folder_${x[i]}
-        y="$y folder_${x[i]},"
+for ((i=0;i<$1;i++)); do
+	mkdir $PWD/folder_${array[i]}
+	output="$output folder_${array[i]},"
 done
-
 if [[ $1 -eq 1 ]]; then
-        y=${y/folders/folder}
-
-        echo "${y/%,/}"
+	output=${output/folders/folder}
+	echo "${output/%,/}"
 else
-        echo "${y/%,/}"
-
+	echo "${output/%,/}"
 fi
