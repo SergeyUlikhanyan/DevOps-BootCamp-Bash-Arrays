face45a35d7a758e5f72771f3cfedf39f5b3d968
Sergey <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-23 15:08:31 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-chmod +x ./array.sh
-count=$1
+x=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+y="$1 folders created:"
 
-read -ra array <<< "$(echo {a..z})"
-
-for ((l=0;l<=count-1;l++))
-
-do
-
-mkdir ./folder_"${array[$l]}"
-
+for ((i=0; i<$1; i++)); do
+        mkdir $PWD/folder_${x[i]}
+        y="$y folder_${x[i]},"
 done
+
+if [[ $1 -eq 1 ]]; then
+        y=${y/folders/folder}
+
+        echo "${y/%,/}"
+else
+        echo "${y/%,/}"
+
+fi
