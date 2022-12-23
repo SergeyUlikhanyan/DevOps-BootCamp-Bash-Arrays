7fc4217b70ac7dc41ad15121f76eb83dff6f44e1
Sergey <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-23 14:26:58 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # Place your code here
+
+count=1
+for i in {a..z}; do
+    [[ $count -gt $1 ]] && break
+    mkdir folder_$i
+    ((count++))
+done
+
+echo  "$1" folder created: && ls | grep folder
