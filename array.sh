84c8a37f81a932f5a9070b0c71ca8d9be6733f7d
Sergey <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-24 15:06:18 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
-
-arg=$1
-declare -a arr
-declare -a res
-count=0
-
+count=1
 for i in {a..z}; do
-        arr+=("$i")
+    [[ $count -gt $1 ]] && break
+    mkdir folder_$i
+    ((count++))
 done
-
-for (( i=0; i < arg; i++ )); do
-
-        if [ "$i" -eq 26 ]; then
-                break
-        fi
-
-        if [ -d folder_"${arr[$i]}" ]; then
-                res+=(folder_"${arr[$i]}")
-                continue
-        fi
-
-        mkdir  folder_"${arr[$i]}"
-        res+=(folder_"${arr[$i]}")
-
-        count=$((count+1))
-
-done
-
-result="${res[*]}"
-if [ $count -le 1 ]; then
-        echo "$count folder created: ${result//${IFS:0:1}/, }"
-else
-        echo "$count folders created: ${result//${IFS:0:1}/, }"
-
-fi
