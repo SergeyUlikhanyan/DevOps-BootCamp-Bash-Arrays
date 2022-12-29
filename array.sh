45f37ed5799c12711adb8eed564edfcdfd52917e
Sergey <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-30 00:41:14 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-count=1
+
+x=$1
+y=1
+
 for i in {a..z}; do
-    [[ $count -gt $1 ]] && break
-    mkdir folder_$i
-    ((count++))
+  if [[ $y -le $x ]]
+  then
+    mkdir ./folder_$i
+    ((y++))
+ fi
 done
+
+if [[ $x -gt 1 ]]
+then
+  echo "$x folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
+else
+  echo "$x folder created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')"
+fi
